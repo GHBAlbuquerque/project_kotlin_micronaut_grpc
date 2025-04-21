dcd03347f298a8da3d7cc28affa9830bbaf09b6e
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-04-21 14:23:38 -0300

    feature: validation class impl

--- src/main/kotlin/br/com/study/resources/ProductResource.kt
+++ src/main/kotlin/br/com/study/resources/ProductResource.kt
@@ -5,6 +5,7 @@ import br.com.study.ProductServiceResponse
 import br.com.study.ProductsServiceGrpc
 import br.com.study.dto.ProductReq
 import br.com.study.services.ProductService
+import br.com.study.utils.ValidationUtil
 import io.grpc.stub.StreamObserver
 import io.micronaut.grpc.annotation.GrpcService
 
@@ -12,7 +13,8 @@ import io.micronaut.grpc.annotation.GrpcService
 class ProductResource(private val productService: ProductService) : ProductsServiceGrpc.ProductsServiceImplBase() {
 
     override fun create(request: ProductServiceRequest?, responseObserver: StreamObserver<ProductServiceResponse>?) {
-        val productReq = ProductReq(name = request!!.name, price = request.price, quantityInStock = request.quantityInStock)
+        val payload = ValidationUtil.isValidPayload(request);
+        val productReq = ProductReq(name = payload.name, price = payload.price, quantityInStock = payload.quantityInStock)
 
         val productRes = productService.create(productReq)
         val response = ProductServiceResponse.newBuilder()
