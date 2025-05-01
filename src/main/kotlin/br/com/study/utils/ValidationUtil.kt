7e7703710ff3cb16cb86b735e182216f93c3cab1
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-05-01 17:34:52 -0300

    test: creates ValidationUtil tests

--- src/main/kotlin/br/com/study/utils/ValidationUtil.kt
+++ src/main/kotlin/br/com/study/utils/ValidationUtil.kt
@@ -30,6 +30,9 @@ class ValidationUtil {
         fun isValidPayload(payload: UpdateProductServiceRequest?): UpdateProductServiceRequest {
 
             payload?.let {
+                if (it.id <= 0L) {
+                    throw InvalidArgumentException("id", "Id must be greater than 0")
+                }
                 if (it.price.isNaN() || it.price < 0) {
                     throw InvalidArgumentException("price", "Price must be greater than 0")
                 }
--- src/test/kotlin/br/com/study/utils/ValidationUtilTest.kt
+++ src/test/kotlin/br/com/study/utils/ValidationUtilTest.kt
@@ -1,6 +1,7 @@
 package br.com.study.utils
 
 import br.com.study.CreateProductServiceRequest
+import br.com.study.UpdateProductServiceRequest
 import br.com.study.exceptions.InvalidArgumentException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test
 class ValidationUtilTest {
 
     @Test
-    fun `when validatePayload method is called with valid data, should not throw exception`() {
+    fun `when validatePayload (Create) method is called with valid data, should not throw exception`() {
         val request = CreateProductServiceRequest.newBuilder()
             .setName("Guaraná")
             .setPrice(9.99)
@@ -22,7 +23,7 @@ class ValidationUtilTest {
     }
 
     @Test
-    fun `when validatePayload method is called with invalid name, should throw exception`() {
+    fun `when validatePayload (Create) method is called with invalid name, should throw exception`() {
         val request = CreateProductServiceRequest.newBuilder()
             .setName("")
             .setPrice(9.99)
@@ -37,7 +38,7 @@ class ValidationUtilTest {
     }
 
     @Test
-    fun `when validatePayload method is called with invalid price, should throw exception`() {
+    fun `when validatePayload (Create) method is called with invalid price, should throw exception`() {
         val request = CreateProductServiceRequest.newBuilder()
             .setName("Product")
             .setPrice(-7.0)
@@ -52,7 +53,7 @@ class ValidationUtilTest {
     }
 
     @Test
-    fun `when validatePayload method is called with invalid quantity in stock, should throw exception`() {
+    fun `when validatePayload (Create) method is called with invalid quantity in stock, should throw exception`() {
         val request = CreateProductServiceRequest.newBuilder()
             .setName("Product")
             .setPrice(7.0)
@@ -67,8 +68,63 @@ class ValidationUtilTest {
     }
 
     @Test
-    fun `when validatePayload method is called with a null payload, should throw exception`() {
+    fun `when validatePayload (Create) method is called with a null payload, should throw exception`() {
         val request : CreateProductServiceRequest? = null
+
+        Assertions.assertThrowsExactly(
+            InvalidArgumentException::class.java
+        ) {
+            ValidationUtil.isValidPayload(request)
+        }
+    }
+
+
+    @Test
+    fun `when validatePayload (Update) method is called with valid data, should not throw exception`() {
+        val request = UpdateProductServiceRequest.newBuilder()
+            .setId(1)
+            .setName("Guaraná")
+            .setPrice(9.99)
+            .setQuantityInStock(5)
+            .build();
+
+        Assertions.assertDoesNotThrow()
+        {
+            ValidationUtil.isValidPayload(request)
+        }
+    }
+
+    @Test
+    fun `when validatePayload (Update) method is called with invalid price, should throw exception`() {
+        val request = UpdateProductServiceRequest.newBuilder()
+            .setId(1)
+            .setPrice(-7.0)
+            .build();
+
+        Assertions.assertThrowsExactly(
+            InvalidArgumentException::class.java
+        ) {
+            ValidationUtil.isValidPayload(request)
+        }
+    }
+
+    @Test
+    fun `when validatePayload (Update) method is called with invalid quantity in stock, should throw exception`() {
+        val request = UpdateProductServiceRequest.newBuilder()
+            .setId(1)
+            .setQuantityInStock(-8)
+            .build();
+
+        Assertions.assertThrowsExactly(
+            InvalidArgumentException::class.java
+        ) {
+            ValidationUtil.isValidPayload(request)
+        }
+    }
+
+    @Test
+    fun `when validatePayload (Update) method is called with a null payload, should throw exception`() {
+        val request : UpdateProductServiceRequest? = null
 
         Assertions.assertThrowsExactly(
             InvalidArgumentException::class.java
